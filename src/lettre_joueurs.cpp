3353b7d538c11464175c64063b3cb770549a4c3b
Clara <clara.carroussel@etu.univ-lyon1.fr>
2020-05-08 16:32:03 +0200

    ajout

--- src/lettre_joueurs.cpp
+++ src/lettre_joueurs.cpp
@@ -1,1 +1,26 @@
+#include "lettre_joueur.hpp"
+#include "sac_de_lettres.hpp"
+using namespace std;
 
+lettres_joueur::lettres_joueur(int nb_lettres_joueur){
+	while (nb_lettres_joueur < 8) //un joueur possède 7 lettres
+	{
+		lettres_joueur.piocher_lettre();
+	}	
+	cout << lettres_joueur << endl;
+}
+
+lettres_joueur::~lettres_joueur(){
+	if ((nb_lettres_joueur == 0) && (nb_lettres == 0))
+	{
+		cout << "Partie terminée" << endl;
+	}	
+	delete;
+}
+
+
+void lettres_joueur::recharger(){
+	if (nb_lettres_joueur < 7)
+		piocher_lettre();
+	cout << lettres_joueur << endl;
+}
--- src/sac_de_lettres.hpp
+++ src/sac_de_lettres.hpp
@@ -1,5 +1,21 @@
 #pragma once
+#include <vector>
 
+/*
 struct sac_de_lettres{
     vector<char> sac_lettre;
-}
+}*/
+
+struct sac_de_lettres{
+	int nb_lettres;
+	/*char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+					'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	std::vector<alphabet> tableau_lettres(lettre); //création d'un tableau de caractère de taille 26 lettres de l'alphabet
+	*/
+	//const char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	//unsigned char tableau_lettres(26);
+
+	sac_de_lettres(int nb_lettres); //constructeur, initialise la sac de lettres 
+	~sac_de_lettres(); //destructeur, détruit le sac de lettres
+};
